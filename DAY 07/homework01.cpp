27565efe85ccf0687a2c86afe42d82c1e82398c2
heyy-kartik <kartikjagdale0511@gmail.com>
2025-07-31 01:47:36 +0530

    GSBGBS

---
+++ DAY 07/homework01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int getAP(int n)
+{
+    int formula = ((3 * n) + 7);
+    return formula;
+}
+
+int main()
+{
+    int formula ;
+    int n;
+    cin >> n;
+    cout << "Sum of the AP : " << formula << endl;
+}
