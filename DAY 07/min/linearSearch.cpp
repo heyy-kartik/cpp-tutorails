c3003e559c50d7fca13c0ffa743c8da615c9412b
heyy-kartik <kartikjagdale0511@gmail.com>
2025-07-31 19:04:42 +0530

    Question Solved

---
+++ DAY 07/min/linearSearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool search(int arr[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == key)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int arr[10] = {2, 5, 7, -3, 9, 34, 12, 45, 23, 43};
+
+    cout << "Enter the Element " << endl;
+
+    int key;
+    cin >> key;
+
+    bool found = search(arr, 10, key);
+    if (found)
+    {
+        cout << " Key is present " << endl;
+    }
+    else
+    {
+        cout << " Key is absent " << endl;
+    }
+
+    return 0;
+}
---
+++ reverseArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int array[], int n) // Fixed: array[] indicates array parameter
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        swap(array[start], array[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int array[], int n)
+{
+
+    for (int i = 0; i < n; i++) // Fixed: i < n instead of i <= n
+    {
+        cout << array[i] << " ";
+    }
+}
+
+int main()
+{
+
+    int array[5] = {34, 2, 76, 8, 5};
+
+    reverse(array, 5);
+    printArray(array, 5);
+    return 0;
+}
