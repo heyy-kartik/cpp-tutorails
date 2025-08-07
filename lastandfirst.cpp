1694b28976895a793bb3f89152a520e72d9def41
heyy-kartik <kartikjagdale0511@gmail.com>
2025-08-07 13:19:37 +0530

    new problem edit

--- lastandfirst.cpp
+++ lastandfirst.cpp
@@ -28,12 +28,39 @@ int firstOccu(int arr[], int n, int key)
     return ans;
 }
 
+int lastOcuurence(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if (arr[mid] == key)
+        {
+            ans = mid;   // store index
+            s = mid + 1; // keep searching in left half
+        }
+        else if (arr[mid] < key)
+        {
+            s = mid + 1; // search in left half
+        }
+        else
+        {
+            e = mid - 1; // search in right half
+        }
+    }
+    return ans;
+}
 int main()
 {
-    int arr[7] = {3, 5, 7, 2, 8, 9, 3};
+    int arr[7] = {2, 3, 3, 5, 7, 8, 9};
 
     sort(arr, arr + 7); // Binary search needs sorted array
 
-    cout << "Index of the first occurrence of 3: " << firstOccu(arr, 7, 3) << endl;
+    cout << "Index of the first occurrence of : " << firstOccu(arr, 7, 3) << endl;
+    cout << "Index of the last  occurrence of: " << lastOcuurence(arr, 7, 3) << endl;
+
     return 0;
 }
